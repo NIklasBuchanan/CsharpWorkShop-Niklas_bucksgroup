d9f23e500525e300f083a729fe9d6b8fedc44356
Niklas21001139 <21001139@student.buckscollegegroup.ac.uk>
2023-06-08 15:03:25 +0100

    Work 08/06/2023
    Today I clone a git rep and some easy console.WriteLines

---
+++ HellowWorld/HellowWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var choice = "Good";
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("You are not");
+Console.WriteLine(choice);
